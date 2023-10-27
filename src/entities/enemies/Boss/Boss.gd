21993d5ed28f94cebdd92cc51547b5331b1cdaa3
Fabian Frangella <fabianfrangella@gmail.com>
2023-10-27 10:41:07 -0300

    agrega hp bar al boss

--- src/entities/enemies/Boss/Boss.gd
+++ src/entities/enemies/Boss/Boss.gd
@@ -2,23 +2,28 @@ extends Node2D
 
 const AnimationState = preload("res://entities/player/AnimationState.gd")
 
-onready var attackTree = get_node("AttackTree")
+onready var attack_tree = get_node("AttackTree")
+onready var heal_timer: Timer = get_node("HealTimer")
+onready var body_anim = $BodyAnimations
 
 var target
+var max_hp = 100
 var hp = 100
 
-onready var body_anim = $BodyAnimations
+signal hp_changed(hp)
 
 func _ready():
 	target = get_parent().get_node("Player")
 	
 func _physics_process(delta):
 	if (target != null):
-		body_anim.set_state(AnimationState.IDLE, _get_direction_to(self.position.direction_to(target.position)))
+		body_anim.set_state(AnimationState.IDLE, 
+		_get_direction_to(self.position.direction_to(target.position)))
 
 func notify_hit():
 	self.hp -= 10
-	attackTree.notify_hit()
+	emit_signal("hp_changed", self.hp, max_hp)
+	attack_tree.notify_hit()
 
 func _get_direction_to(pos):
 	var abs_position = Vector2(0, 0)
@@ -31,3 +36,12 @@ func _get_direction_to(pos):
 	if (pos.y < 0):
 		abs_position.y = -1
 	return abs_position
+
+
+func _on_HealTimer_timeout():
+	if (hp < 100):
+		hp+=10
+		if (hp > max_hp):
+			hp = max_hp
+		emit_signal("hp_changed", hp, max_hp)
+		
