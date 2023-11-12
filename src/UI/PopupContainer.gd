d004820a579cc997d8ef198d1f52ad0ebe088c3a
Fabian Frangella <fabianfrangella@gmail.com>
2023-11-12 12:58:00 -0300

    tutorial sin responsive

--- src/UI/PopupContainer.gd
+++ src/UI/PopupContainer.gd
@@ -5,6 +5,7 @@ onready var continue_button = $ContinueButton
 onready var range_enemy_text = $RangeEnemyText
 onready var melee_enemy_text = $MeleeEnemyText
 onready var potion_text = $PotionText
+onready var boss_text = $BossText
 
 var range_enemy_triggered = false
 var melee_enemy_triggered = false
@@ -17,6 +18,7 @@ func _ready():
 	range_enemy_text.hide()
 	melee_enemy_text.hide()
 	potion_text.hide()
+	boss_text.hide()
 
 func _on_RangeEnemyArea_area_entered(area):
 	if (area.name == "PlayerHitbox"):
@@ -51,3 +53,22 @@ func _on_PotionArea_area_entered(area):
 			potion_triggered = true
 			get_tree().paused = true
 			continue_button.show()
+
+
+func _on_BossChamberArea_area_entered(area):
+	if (area.name == "PlayerHitbox"):
+		var text = ""
+		if (not area.get_parent().hasGSword):
+			text += "Parece que aún no has encontrado una buena espada. \n"
+		if (not area.get_parent().hasStaff):
+			text += "Todavía no has encontrado tu baculo? \n"
+		if (not area.get_parent().hasArmor):
+			text += "Sin armadura?, buena suerte \n"
+		if (text != ""):
+			text += "Si no tienes los elementos necesarios será muy dificil que mates al demonio"
+			boss_text.text = text
+			current_hint = boss_text
+			boss_text.show()
+			get_tree().paused = true
+			continue_button.show()
+		
