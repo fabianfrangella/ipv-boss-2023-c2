7f906a9b7a3c91d5e863a906c2d29fb2775811e3
Fabian Frangella <fabianfrangella@gmail.com>
2023-11-12 12:47:49 -0300

    tutorial in progress

--- src/UI/PopupContainer.gd
+++ src/UI/PopupContainer.gd
@@ -4,15 +4,19 @@ extends Control
 onready var continue_button = $ContinueButton
 onready var range_enemy_text = $RangeEnemyText
 onready var melee_enemy_text = $MeleeEnemyText
+onready var potion_text = $PotionText
 
 var range_enemy_triggered = false
 var melee_enemy_triggered = false
+var potion_triggered = false
+
 onready var current_hint = $RangeEnemyText
 
 func _ready():
 	continue_button.hide()
 	range_enemy_text.hide()
 	melee_enemy_text.hide()
+	potion_text.hide()
 
 func _on_RangeEnemyArea_area_entered(area):
 	if (area.name == "PlayerHitbox"):
@@ -35,5 +39,15 @@ func _on_MeleeEnemyArea_area_entered(area):
 			current_hint = melee_enemy_text
 			melee_enemy_text.show()
 			melee_enemy_triggered = true
+			get_tree().paused = true
+			continue_button.show()
+
+
+func _on_PotionArea_area_entered(area):
+	if (area.name == "PlayerHitbox"):
+		if (not potion_triggered):
+			current_hint = potion_text
+			potion_text.show()
+			potion_triggered = true
 			get_tree().paused = true
 			continue_button.show()
