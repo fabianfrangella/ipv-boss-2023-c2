684b3aaf41c9700748f802b7dd535e9bf75d1800
Fabian Frangella <fabianfrangella@gmail.com>
2023-11-13 12:41:48 -0300

    range attack sound

--- src/entities/player/RangeAttack.gd
+++ src/entities/player/RangeAttack.gd
@@ -2,6 +2,7 @@ extends AttackMode
 
 signal player_shoot
 
+var sound = preload("res://Sounds/Spell/Magic Spell_Simple Swoosh_6.wav")
 func _handle_attack(body: KinematicBody2D) -> void:
 	if Input.is_action_just_pressed("attack") && body.mana >= 2.0:
 		if body.projectile_container == null:
@@ -11,3 +12,9 @@ func _handle_attack(body: KinematicBody2D) -> void:
 		body.range_weapon.fire()
 		body.body_anim.set_state('range')
 		self.can_attack = false
+		_play_sound(body)
+		
+func _play_sound(body):
+	var player = body.audio_container.get_node("Spell")
+	player.stream = sound
+	player.play()
