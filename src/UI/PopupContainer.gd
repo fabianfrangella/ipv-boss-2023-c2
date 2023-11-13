d98276efe7afc974afe92a3fa91ca0b1b2efc8da
Fabian Frangella <fabianfrangella@gmail.com>
2023-11-12 22:28:11 -0300

    tutorial responsive

--- src/UI/PopupContainer.gd
+++ src/UI/PopupContainer.gd
@@ -1,11 +1,12 @@
 extends Control
 
 
-onready var continue_button = $ContinueButton
-onready var range_enemy_text = $RangeEnemyText
-onready var melee_enemy_text = $MeleeEnemyText
-onready var potion_text = $PotionText
-onready var boss_text = $BossText
+onready var continue_button = $Panel/ContinueButton
+onready var range_enemy_text = $Panel/RangeEnemyText
+onready var melee_enemy_text = $Panel/MeleeEnemyText
+onready var potion_text = $Panel/PotionText
+onready var boss_text = $Panel/BossText
+onready var panel = $Panel
 
 var range_enemy_triggered = false
 var melee_enemy_triggered = false
@@ -19,26 +20,29 @@ func _ready():
 	melee_enemy_text.hide()
 	potion_text.hide()
 	boss_text.hide()
+	panel.hide()
 
 func _on_RangeEnemyArea_area_entered(area):
 	if (area.name == "PlayerHitbox"):
 		if (not range_enemy_triggered):
 			current_hint = range_enemy_text
+			panel.show()
 			range_enemy_text.show()
 			range_enemy_triggered = true
 			get_tree().paused = true
 			continue_button.show()
 
 func _on_ContinueButton_pressed():
-	print("continuar")
 	get_tree().paused = false
 	current_hint.hide()
 	continue_button.hide()
+	panel.hide()
 
 func _on_MeleeEnemyArea_area_entered(area):
 	if (area.name == "PlayerHitbox"):
 		if (not melee_enemy_triggered):
 			current_hint = melee_enemy_text
+			panel.show()
 			melee_enemy_text.show()
 			melee_enemy_triggered = true
 			get_tree().paused = true
@@ -49,6 +53,7 @@ func _on_PotionArea_area_entered(area):
 	if (area.name == "PlayerHitbox"):
 		if (not potion_triggered):
 			current_hint = potion_text
+			panel.show()
 			potion_text.show()
 			potion_triggered = true
 			get_tree().paused = true
@@ -68,6 +73,7 @@ func _on_BossChamberArea_area_entered(area):
 			text += "Si no tienes los elementos necesarios será muy dificil que mates al demonio"
 			boss_text.text = text
 			current_hint = boss_text
+			panel.show()
 			boss_text.show()
 			get_tree().paused = true
 			continue_button.show()
