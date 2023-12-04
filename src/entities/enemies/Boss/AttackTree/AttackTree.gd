3a4466b95c22524f83e521e288244bef90ed59ad
Fabian Frangella <fabianfrangella@gmail.com>
2023-12-04 17:32:39 -0300

    fix boss movement

--- src/entities/enemies/Boss/AttackTree/AttackTree.gd
+++ src/entities/enemies/Boss/AttackTree/AttackTree.gd
@@ -9,6 +9,8 @@ onready var fireAttack = get_node("FireAttack")
 onready var summonAttack = get_node("SummonAttack")
 onready var states = [areaAttack]
 
+var next_portal = 0
+
 func _physics_process(delta):
 	if (boss.is_dead() || boss.move_to_portal):
 		return
@@ -28,18 +30,23 @@ func _set_states(hp):
 	elif (hp <= 90 && boss.hp > 70):
 		states = [fireAttack]
 	elif (hp <= 70 && boss.hp > 50):
-		boss.move_to_portal = true
-		boss.current_portal = 1
+		if (next_portal < 1):
+			_move_to_next_portal()
 		states = [summonAttack]
 	elif (hp <= 50 && boss.hp > 30):
 		states = [areaAttack, fireAttack]
 	elif (hp <= 30 && boss.hp > 10):
-		boss.move_to_portal = true
-		boss.current_portal = 2
+		if (next_portal < 2):
+			_move_to_next_portal()
 		states = [fireAttack, areaAttack, summonAttack]
 	elif (hp <= 10 && boss.hp > 0):
-		boss.move_to_portal = true
-		boss.current_portal = 3
+		if (next_portal < 3):
+			_move_to_next_portal()
 		states = [fireAttack, areaAttack, summonAttack]
 	elif (hp <= 0):
 		states = []
+
+func _move_to_next_portal():
+	next_portal += 1
+	boss.move_to_portal = true
+	boss.current_portal = next_portal
