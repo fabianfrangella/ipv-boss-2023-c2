525d22df3970c8f0dee03a3dcb18820d59e43d90
Fabian Frangella <fabianfrangella@gmail.com>
2023-12-14 15:50:05 -0300

    fix summon attack

--- src/entities/enemies/Boss/AttackTree/SummonAttack.gd
+++ src/entities/enemies/Boss/AttackTree/SummonAttack.gd
@@ -10,10 +10,10 @@ var can_summon = true
 func handle_state(delta):
 	if (can_summon):
 		can_summon = false
-		_summon(enemies_container.get_node("Enemy_1").position, "Enemy_1", "range")
-		_summon(enemies_container.get_node("Enemy_2").position, "Enemy_2", "range")
-		_summon(enemies_container.get_node("Enemy_3").position, "Enemy_3", "melee")
-		_summon(enemies_container.get_node("Enemy_4").position, "Enemy_4", "melee")
+		_summon(enemies_container.get_node("Enemy_1").global_position, "Enemy_1", "range")
+		_summon(enemies_container.get_node("Enemy_2").global_position, "Enemy_2", "range")
+		_summon(enemies_container.get_node("Enemy_3").global_position, "Enemy_3", "melee")
+		_summon(enemies_container.get_node("Enemy_4").global_position, "Enemy_4", "melee")
 
 func _summon(summon_position, enemy_name, name):
 	var enemy
@@ -24,8 +24,7 @@ func _summon(summon_position, enemy_name, name):
 		enemy.pathfinding_path = get_parent().boss.pathfinding_path 
 	enemy.name = enemy_name
 	enemy.global_position = summon_position
-	enemy.scale = Vector2(0.20, 0.20)
-	add_child(enemy)
+	get_parent().boss.get_parent().add_child(enemy)
 
 func _on_Timer_timeout():
 	can_summon = true
